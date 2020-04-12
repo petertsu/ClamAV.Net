5a227f36e77bad4e12c56360b9a05b3bdac075a6
Peter Tsuprun <Peter.Tsuprun@votiro.com>
2020-04-12 18:07:56 +0300

    Tests

--- tests/ClamAV.Net.Tests/Commands/PingCommandTests.cs
+++ tests/ClamAV.Net.Tests/Commands/PingCommandTests.cs
@@ -1,4 +1,8 @@
-﻿using ClamAV.Net.Commands;
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using ClamAV.Net.ClamdProtocol;
+using ClamAV.Net.Commands;
 using FluentAssertions;
 using Xunit;
 
@@ -11,6 +15,22 @@ namespace ClamAV.Net.Tests.Commands
         {
             PingCommand pingCommand = new PingCommand();
             pingCommand.Name.Should().Be("PING");
+        }
+
+        [Fact]
+        public async Task WriteCommandAsync_Should_Write_CommandName()
+        {
+            PingCommand pingCommand = new PingCommand();
+            await using MemoryStream memoryStream = new MemoryStream();
+
+            await pingCommand.WriteCommandAsync(memoryStream).ConfigureAwait(false);
+
+            byte[] commandData = memoryStream.ToArray();
+
+            string actual = Encoding.UTF8.GetString(commandData);
+
+            actual.Should()
+                .Be($"{Consts.COMMAND_PREFIX_CHARACTER}{pingCommand.Name}{(char)Consts.TERMINATION_BYTE}");
         }
     }
 }
