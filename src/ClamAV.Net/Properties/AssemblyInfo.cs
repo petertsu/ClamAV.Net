c6d7e133c75ac0f1c6aadbea55a6b36d70adc261
Peter Tsuprun <Peter.Tsuprun@votiro.com>
2020-04-12 17:45:25 +0300

    Tests

---
+++ src/ClamAV.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("ClamAV.Net.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
---
+++ tests/ClamAV.Net.Tests/Commands/PingCommandTests.cs
@@ -0,0 +1,16 @@
+﻿using ClamAV.Net.Commands;
+using FluentAssertions;
+using Xunit;
+
+namespace ClamAV.Net.Tests.Commands
+{
+    public class PingCommandTests
+    {
+        [Fact]
+        public void Ctor_Set_Valid_Name()
+        {
+            PingCommand pingCommand = new PingCommand();
+            pingCommand.Name.Should().Be("PING");
+        }
+    }
+}
